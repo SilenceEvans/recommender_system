5c1f8fe15f44b3951038ef66ad756f63da9e77b2
SilenceEvans <coderdixin@qq.com>
2022-09-14 22:46:49 +0800

    实现deep_cross

---
+++ dcn/dcn_model.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：rec_models 
+@File    ：dcn_model.py
+@Author  ：ChenxiWang
+@Date    ：2022/9/14 8:54 下午 
+@Github  : https://github.com/SilenceEvans/recommender_system
+@Description :
+"""
+import torch
+import torch.nn as nn
+from dataset import get_sparse_fea_num
+import torch.nn.functional as F
+
+from dcn import config
+
+
+class Cross(nn.Module):
+    def __init__(self, num_layers, cross_dim):
+        super(Cross, self).__init__()
+        self.num_layers = num_layers
+        # 在这依然像之前pnn中一样采用初始化w参数矩阵的方式，而是直接定义了线性层，在我目前来看这样更为清晰明了
+        self.linear = nn.ModuleList([nn.Linear(cross_dim, 1) for _ in range(num_layers)])
+        self.bias = nn.ParameterList(
+            [nn.Parameter(nn.init.zeros_(torch.empty(cross_dim,1))) for _ in range(num_layers)])
+
+    def forward(self, x):
+        x_0 = x.unsqueeze(2)
+        x_l = x_0
+        for i in range(self.num_layers):
+            tmp = x_0.matmul(x_l.transpose(2, 1)).to(torch.float32)
+            tmp = self.linear[i](tmp)
+            x_l = tmp + self.bias[i] + x_l
+
+        return x_l.squeeze(2)
+
+
+class DNN(nn.Module):
+    def __init__(self, hidden_units, drop_out):
+        super(DNN, self).__init__()
+        self.dnn_network = nn.ModuleList(
+            [nn.Linear(layer[0], layer[1]) for layer in list(zip(hidden_units[:-1], hidden_units[1:]))])
+        self.drop_out = nn.Dropout(drop_out)
+
+    def forward(self, x):
+        x = x.float()
+        for linear in self.dnn_network:
+            x = linear(x)
+            x = F.relu(x)
+        return self.drop_out(x)
+
+
+class DeepCross(nn.Module):
+    def __init__(self, hidden_units=config.hidden_units, drop_out=config.dropout,
+                 embedding_dim=config.embedding_dim):
+        super(DeepCross, self).__init__()
+        self.sparse_fea_num, self.sparse, self.dense = get_sparse_fea_num()
+        self.embedding_layers = nn.ModuleDict(
+            {'embedding_' + str(key): nn.Embedding(val, embedding_dim) for key, val in
+             self.sparse_fea_num.items()})
+        dnn_cross_dim = len(self.sparse) * embedding_dim + len(self.dense)
+        hidden_units_copy = hidden_units.copy()
+        hidden_units_copy.insert(0,dnn_cross_dim)
+        self.dnn = DNN(hidden_units=hidden_units_copy, drop_out=drop_out)
+        self.cross = Cross(2, dnn_cross_dim)
+        self.final_linear = nn.Linear(dnn_cross_dim + hidden_units_copy[-1], 1)
+
+    def forward(self, x):
+        dense_inputs, sparse_inputs = x[:, :13], x[:, 13:]
+        sparse_inputs = sparse_inputs.long()
+        sparse_embedded = [self.embedding_layers['embedding_' + col_name](sparse_inputs[:, col_val]) for
+                           col_name, col_val in
+                           zip(self.sparse_fea_num.keys(), range(sparse_inputs.shape[1]))]
+        sparse_embedded = torch.cat(sparse_embedded, dim=-1)
+        cross_deep_inputs = torch.cat((dense_inputs, sparse_embedded), dim=-1)
+
+        # cross
+        cross_outputs = self.cross(cross_deep_inputs)
+        deep_outputs = self.dnn(cross_deep_inputs)
+
+        cd_outputs = self.final_linear(torch.cat((cross_outputs, deep_outputs), dim=-1).float())
+
+        return torch.sigmoid(cd_outputs).squeeze(-1)
--- wide_deep/wide_deep_model.py
+++ wide_deep/wide_deep_model.py
@@ -8,3 +8,66 @@
 @Github  : https://github.com/SilenceEvans/recommender_system
 @Description :
 """
+import torch
+import torch.nn as nn
+from dataset import get_sparse_fea_num
+import config
+import torch.nn.functional as F
+
+
+class Linear(nn.Module):
+    def __init__(self, input_dim):
+        super(Linear, self).__init__()
+        self.linear = nn.Linear(in_features=input_dim, out_features=1)
+
+    def forward(self, x):
+        x = x.float()
+        return self.linear(x)
+
+
+class DNN(nn.Module):
+    def __init__(self, hidden_units, drop_out):
+        super(DNN, self).__init__()
+        self.dnn_network = nn.ModuleList(
+            [nn.Linear(layer[0], layer[1]) for layer in list(zip(hidden_units[:-1], hidden_units[1:]))])
+        self.drop_out = nn.Dropout(drop_out)
+
+    def forward(self, x):
+        x = x.float()
+        for linear in self.dnn_network:
+            x = linear(x)
+            x = F.relu(x)
+        return self.drop_out(x)
+
+
+class WideDeep(nn.Module):
+
+    def __init__(self, hidden_units=config.hidden_units, drop_out=config.dropout):
+        super(WideDeep, self).__init__()
+        self.sparse_fea_num, self.sparse, self.dense = get_sparse_fea_num()
+        # embedding
+        self.embed_layers = nn.ModuleDict(
+            {'embed_' + str(key): nn.Embedding(val, config.embedding_dim) for key, val in
+             self.sparse_fea_num.items()})
+        hidden_units_copy = hidden_units.copy()
+        hidden_units_copy.insert(0, len(self.dense) + (len(self.sparse) * config.embedding_dim))
+        self.dnn = DNN(hidden_units_copy, drop_out)
+        self.linear = Linear(len(self.dense))
+        self.final_linear = nn.Linear(hidden_units_copy[-1], 1)
+
+    def forward(self, x):
+        dense_inputs, sparse_inputs = x[:, :13], x[:, 13:]
+        sparse_inputs = sparse_inputs.long()
+        sparse_embedded = [self.embed_layers['embed_' + col_name](sparse_inputs[:, col_num]) for
+                           col_name, col_num in
+                           zip(self.sparse_fea_num.keys(), range(sparse_inputs.shape[1]))]
+        dnn_inputs = torch.cat((torch.cat(sparse_embedded, dim=-1), dense_inputs), dim=-1)
+        dnn_outputs = self.dnn(dnn_inputs)
+        deep_outputs = self.final_linear(dnn_outputs)
+
+        # wide
+        wide_outputs = self.linear(dense_inputs)
+
+        # wide & deep
+
+        return torch.sigmoid(0.5 * (deep_outputs + wide_outputs)).squeeze(-1)

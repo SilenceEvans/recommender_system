a200157e88b4b1192d1ccb08068bc62bf6c9b084
SilenceEvans <coderdixin@qq.com>
2022-09-14 20:47:55 +0800

    实现deep&wide

---
+++ wide_deep/wide_deep_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：rec_models 
+@File    ：wide_deep_model.py
+@Author  ：ChenxiWang
+@Date    ：2022/9/14 5:51 下午 
+@Github  : https://github.com/SilenceEvans/recommender_system
+@Description :
+"""
